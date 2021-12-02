888bd264adafffe17d80a9cc7acba7dd4c9ada8a
Bea Stollnitz <bea@stollnitz.com>
2021-12-02 14:08:49 -0800

    Improved sample request creation.

--- managed-endpoint/managed-endpoint/pytorch-src/create_sample_request.py
+++ managed-endpoint/managed-endpoint/pytorch-src/create_sample_request.py
@@ -2,22 +2,26 @@
 
 import json
 import os
+from pathlib import Path
 
 from train import _get_data
+
+DATA_PATH = 'managed-endpoint/data'
+SAMPLE_REQUEST = 'managed-endpoint/sample-request'
 
 
 def create_sample_request() -> None:
     """Creates a sample request to be used in prediction."""
     batch_size = 64
-    dir_name = 'managed-endpoint/sample-request'
-    file_name = dir_name + '/sample_request.json'
     (_, test_dataloader) = _get_data(batch_size)
 
     (x_batch, _) = next(iter(test_dataloader))
     x = x_batch[0, 0, :, :].cpu().numpy().tolist()
 
-    os.makedirs(name=dir_name, exist_ok=True)
-    with open(file_name, 'w', encoding='utf-8') as file:
+    os.makedirs(name=SAMPLE_REQUEST, exist_ok=True)
+    with open(Path(SAMPLE_REQUEST, 'sample_request.json'),
+              'w',
+              encoding='utf-8') as file:
         json.dump({'data': x}, file)
 
 
