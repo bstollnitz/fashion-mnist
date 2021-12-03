f12702f399449a5050c88c96f0d6b631914640ac
Bea Stollnitz <bea@stollnitz.com>
2021-12-02 22:25:39 -0800

    Added rest script files.

---
+++ managed-endpoint/managed-endpoint/rest/invoke_aml_token.sh
@@ -0,0 +1,13 @@
+ENDPOINT_NAME=endpoint-managed-fashion-5
+
+SCORING_URI=$(az ml online-endpoint show --name $ENDPOINT_NAME --query scoring_uri -o tsv)
+echo "SCORING_URI: $SCORING_URI"
+
+ACCESS_TOKEN=$(az ml online-endpoint get-credentials --name $ENDPOINT_NAME --query accessToken -o tsv)
+echo "PRIMARY_KEY: $ACCESS_TOKEN"
+
+OUTPUT=$(curl --location --request POST $SCORING_URI \
+     --header "Authorization: Bearer $ACCESS_TOKEN" \
+     --header "Content-Type: application/json" \
+     --data @managed-endpoint/sample-request/sample_request.json)
+echo "OUTPUT: $OUTPUT"
---
+++ managed-endpoint/managed-endpoint/rest/invoke_key.sh
@@ -0,0 +1,14 @@
+ENDPOINT_NAME=endpoint-managed-fashion-1
+
+SCORING_URI=$(az ml online-endpoint show --name $ENDPOINT_NAME --query scoring_uri -o tsv)
+echo "SCORING_URI: $SCORING_URI"
+
+PRIMARY_KEY=$(az ml online-endpoint get-credentials --name $ENDPOINT_NAME --query primaryKey -o tsv)
+echo "PRIMARY_KEY: $PRIMARY_KEY"
+
+OUTPUT=$(curl --location \
+     --request POST $SCORING_URI \
+     --header "Authorization: Bearer $PRIMARY_KEY" \
+     --header "Content-Type: application/json" \
+     --data @managed-endpoint/sample-request/sample_request.json)
+echo "OUTPUT: $OUTPUT"
